491c15021baf04bc5031e523972cc0740cd36451
sketch <einfach2live@foxmail.com>
2022-06-24 18:48:48 +0800

    第二次更新

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -8,5 +8,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("first update!");
+        System.out.println("second update!");
     }
 }
