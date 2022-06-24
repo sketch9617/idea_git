9d1f131ae594dd3dfb00de5306a75b9fc31f79e3
sketch <einfach2live@foxmail.com>
2022-06-24 18:47:49 +0800

    第一次更新

---
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author sketch
+ * @date 2022/6/24 18:42
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("first update!");
+    }
+}
