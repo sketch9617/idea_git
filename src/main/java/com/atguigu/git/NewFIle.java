0d9f7ee4c4c2ce95ef0819936ea6e4e311345776
sketch-idea <einfach2live@foxmail.com>
2022-06-24 18:54:14 +0800

    第四次更新

--- src/main/java/com/atguigu/git/NewFIle.java
+++ src/main/java/com/atguigu/git/NewFIle.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class NewFIle {
     public static void main(String[] args) {
         System.out.println("new file");
+        System.out.println("change name");
     }
 }
